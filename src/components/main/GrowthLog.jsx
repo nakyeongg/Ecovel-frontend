5f1f9e2c288125a587a4dc1b510d5f3411d14990
nakyeong <yunna3425@naver.com>
2025-05-14 22:39:28 +0900

    Feat: 성장 로그 api 연결

--- src/components/main/GrowthLog.jsx
+++ src/components/main/GrowthLog.jsx
@@ -8,16 +8,26 @@ import crown from '../../assets/icons/main/crown.png';
 import mainAxios from './../../apis/mainAxios';
 
 export const GrowthLog = ({ userId }) => {
-    const [growthData, setGrowthData] = useState();
-    const [totalCarbonSaved, setTotalCarbonSaved] = useState(125.0);
+    const [totalCarbonSaved, setTotalCarbonSaved] = useState(0);
+    const [growthStage, setGrowthStage] = useState('씨앗');
+    const [pointStatus, setPointStatus] = useState(0); // 포인트 현황
+    const [totalPoint, setTotalPoint] = useState(5); // 이번 growthStage에서의 최대 포인트
 
     const handleGrowthLog = async () => {
         try {
-            console.log('성장로그 아이디', userId);
+            console.log('userId: ', userId);
             const response = await mainAxios.get(`/growth/log?userId=${userId}`);
             console.log('성장 로그 요청 성공', response);
-            // setGrowthData(response.result);
-            // setTotalCarbonSaved(response.result.totalCarbonSaved);
+            setTotalCarbonSaved(response.data.result.totalCarbonSaved);
+            setGrowthStage(response.data.result.growthStage);
+            const tempPointStatus = response.data.result.totalCarbonSaved + response.data.result.totalMissionSuccessCount + response.data.result.totalQuizSuccessCount>=10 ? 10 : response.data.result.totalCarbonSaved + response.data.result.totalMissionSuccessCount + response.data.result.totalQuizSuccessCount;
+            setPointStatus(tempPointStatus);
+            console.log('포인트 합계: ', tempPointStatus);
+            if (tempPointStatus < 5) {
+                setTotalPoint(5);
+            } else {
+                setTotalPoint(10);
+            }
         } catch(error) {
             console.log('성장 로그 요청 실패', error);
         }
@@ -33,11 +43,17 @@ export const GrowthLog = ({ userId }) => {
         <Wrapper>
             <GraphWrapper>
                 <GraphBack>
-                    <Graph />
+                    <Graph width={(pointStatus/totalPoint)*100}/>
                 </GraphBack>
                 <p>I saved {totalCarbonSaved}kg of carbon</p>
             </GraphWrapper>
-            <Image src={tree} />
+            {growthStage==="씨앗" ? (
+                <Image src={seed} />
+            ) : growthStage==="새싹" ? (
+                <Image src={sprout} />
+            ) : growthStage==="성장기" ? (
+                <Image src={tree} />
+            ) : null}
         </Wrapper>
     )
 }
@@ -65,7 +81,7 @@ const GraphBack = styled.div`
 
 const Graph = styled.div`
     background-color: #2DCF73;
-    width: 110px;
+    width: ${({width}) => width}%;
     height: 15px;
 `
 
